652a442362e95d1cc37f73857b60607bc7c76516
Sophie Lin <98501518+soph-lin@users.noreply.github.com>
2025-10-26 14:18:06 -0500

    Initial Commit

---
+++ pebble/pebbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  pebbleApp.swift
+//  pebble
+//
+//  Created by Sophie Lin on 10/26/25.
+//
+
+import SwiftUI
+
+@main
+struct pebbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
